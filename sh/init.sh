afa2d9187aee5947bbf3b9814b3ccf28d8dba51e
尤雨东 <root@ltpp.vip>
2024-05-01 10:35:26 +0800

    feat:init

---
+++ sh/init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git remote add ltpp ssh://git@git.ltpp.vip:66/root/RUST-WEB-SERVE.git;
+git remote add github git@github.com:wmzn-ltpp/RUST-WEB-SERVE.git;
---
+++ sh/push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+git config --global core.autocrlf input;
+git add .;
+echo -n "Please enter the update message and press Enter:"
+read update_message
+if [ -z "$update_message" ]; then
+    echo "The update message cannot be empty. Please rerun the script and provide a valid update message."
+    echo "Press Enter to exit...";
+    read -n 1;
+    exit 1
+fi
+git commit -m "feat:$update_message";
+git push github master;
+echo -e "\e[32mgithub push finish\e[0m";
+git push ltpp master;
+echo -e "\e[32mltpp push finish\e[0m";
+echo "Press Enter to continue...";
+read -n 1;
