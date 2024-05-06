4386663587bef55266264506cac8ae324edb36e0
尤雨东 <root@ltpp.vip>
2024-05-06 23:56:16 +0800

    feat:update

--- sh/push.sh
+++ sh/push.sh
@@ -14,5 +14,7 @@ git push github master;
 echo -e "\e[32mgithub push finish\e[0m";
 git push ltpp master;
 echo -e "\e[32mltpp push finish\e[0m";
+git push origin master;
+echo -e "\e[32morigin push finish\e[0m";
 echo "Press Enter to continue...";
 read -n 1;
