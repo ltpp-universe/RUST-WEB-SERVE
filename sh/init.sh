9dfeff7c7adfb77b775d5a502c49055850d37e6f
尤雨东 <root@ltpp.vip>
2024-07-16 22:15:28 +0800

    feat:update

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git remote add ltpp ssh://git@git.ltpp.vip:66/root/RUST-WEB-SERVE.git;
 git remote add github git@github.com:ltpp-universe/RUST-WEB-SERVE.git;
+git remote add origin git@github.com:ltpp-universe/RUST-WEB-SERVE.git;
