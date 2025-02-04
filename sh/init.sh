94a9a9167f9d89fec3ad6a082efe7bb3ba81f3bf
尤雨东 <root@ltpp.vip>
2025-02-04 10:29:30 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git remote add jihulab ssh://jihulab.com:ltpp/RUST-WEB-SERVE.git;
+git remote add jihulab git@jihulab.com:ltpp-universe/RUST-WEB-SERVE.git;
 git remote add github git@github.com:ltpp-universe/RUST-WEB-SERVE.git;
 git remote add origin git@github.com:ltpp-universe/RUST-WEB-SERVE.git;
