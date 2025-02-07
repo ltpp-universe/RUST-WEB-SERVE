7ba9ef06bc219f134d3c0a5be6ec8dfdffd3744a
尤雨东 <root@ltpp.vip>
2025-02-07 22:53:54 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git remote add jihulab git@jihulab.com:ltpp-universe/RUST-WEB-SERVE.git;
+git remote add gitee git@gitee.com:ltpp-universe/RUST-WEB-SERVE.git;
 git remote add github git@github.com:ltpp-universe/RUST-WEB-SERVE.git;
 git remote add origin git@github.com:ltpp-universe/RUST-WEB-SERVE.git;
--- sh/push.sh
+++ sh/push.sh
@@ -12,8 +12,8 @@ fi
 git commit -m "feat:$update_message";
 git push github master;
 echo -e "\e[32mgithub push finish\e[0m";
-git push jihulab master;
-echo -e "\e[32mjihulab push finish\e[0m";
+git push gitee master;
+echo -e "\e[32mgitee push finish\e[0m";
 git push origin master;
 echo -e "\e[32morigin push finish\e[0m";
 echo "Press Enter to continue...";
