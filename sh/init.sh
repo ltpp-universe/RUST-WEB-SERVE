4c0510be1c41eda40ea7e8a5ea2daf561643d1da
尤雨东 <root@ltpp.vip>
2025-02-09 21:34:00 +0800

    n

--- sh/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git init;
-git config core.ignorecase false;
-git remote add gitee git@gitee.com:ltpp-universe/RUST-WEB-SERVE.git;
-git remote add origin git@github.com:ltpp-universe/RUST-WEB-SERVE.git;
